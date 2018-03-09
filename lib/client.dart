8c7a8664cc0826af33cddd9c75debf6798bec2b6
Tobe O <thosakwe@gmail.com>
2018-03-09 18:53:51 -0500

    Remove check for contentLength when sending a body; the http package doesn't send one, which means that POST requests are always improperly signed.

--- lib/client.dart
+++ lib/client.dart
@@ -9,16 +9,17 @@ import 'package:oauth/src/utils.dart';
 import 'package:oauth/src/core.dart';
 import 'package:oauth/src/token.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 export 'package:oauth/src/token.dart' show Tokens;
 
 const Base64Codec _base64 = const Base64Codec();
 
 /** Generate the parameters to be included in the `Authorization:` header of a
- *  request. Generally you should prefer use of [signRequest] or [Client] 
+ *  request. Generally you should prefer use of [signRequest] or [Client]
  */
 Map<String, String> generateParameters(
     http.Request request,
-    Tokens tokens, 
+    Tokens tokens,
     String nonce,
     int timestamp) {
   Map<String, String> params = new SplayTreeMap<String, String>();
@@ -26,25 +27,26 @@ Map<String, String> generateParameters(
   if(tokens.userId != null) {
     params["oauth_token"] = tokens.userId;
   }
-  
+
   params["oauth_signature_method"] = tokens.type;
   params["oauth_version"] = "1.0";
   params["oauth_nonce"] = nonce;
   params["oauth_timestamp"] = timestamp.toString();
-  
+
   List<Parameter> requestParams = new List<Parameter>();
   requestParams.addAll(mapParameters(request.url.queryParameters));
   requestParams.addAll(mapParameters(params));
-  
-  if(request.contentLength != null
-      && request.contentLength != 0
-      && ContentType.parse(request.headers["content-Type"]).mimeType == "application/x-www-form-urlencoded") {
+
+  var mediaType = request.headers.containsKey('content-type') ? new MediaType.parse(request.headers['content-type']) : null;
+
+  if(mediaType?.type == 'application'
+      && mediaType?.subtype == 'x-www-form-urlencoded') {
     requestParams.addAll(mapParameters(request.bodyFields));
-  } 
-  
+  }
+
   var sigBase = computeSignatureBase(request.method, request.url, requestParams);
   params["oauth_signature"] = _base64.encode(tokens.sign(sigBase));
-  
+
   return params;
 }
 
@@ -56,47 +58,47 @@ String produceAuthorizationHeader(Map<String, String> parameters) {
 /** Signs [request] using consumer token [consumerToken] and user authorization
  *  [userToken]
  *
- *  If the body of [request] has content type 
- *  `application/x-www-form-urlencoded`, then the request body cannot be 
+ *  If the body of [request] has content type
+ *  `application/x-www-form-urlencoded`, then the request body cannot be
  *  streaming as the body parameters are required as part of the signature.
- * 
+ *
  *  The combination of [consumerToken], [userToken], [nonce] and [timestamp]
- *  must be unique. [timestamp] must be specified in Unix time format (i.e. 
+ *  must be unique. [timestamp] must be specified in Unix time format (i.e.
  *  seconds since 1970-01-01T00:00Z)
  */
 void signRequest(http.BaseRequest request,
                  Tokens tokens,
                  String nonce,
                  int timestamp) {
-  
+
   var params = generateParameters(request, tokens, nonce, timestamp);
-  
+
   request.headers["Authorization"] = produceAuthorizationHeader(params);
 }
 
 /** An implementation of [http.BaseClient] which signs all requests with the
  * provided credentials.
- * 
+ *
  */
 class Client extends http.BaseClient {
   /// The OAuth tokens. Required.
   Tokens tokens;
   http.BaseClient _client;
-  
+
   /// The wrapped client
   http.BaseClient get client => _client;
-  
-  /** Constructs a new client, with tokens [consumerToken] and optionally 
-   * [userToken]. If [client] is provided, it will be wrapped, else a new 
+
+  /** Constructs a new client, with tokens [consumerToken] and optionally
+   * [userToken]. If [client] is provided, it will be wrapped, else a new
    * [http.Client] will be created.
-   * 
-   *  If the body of any request has content type 
-   * `application/x-www-form-urlencoded`, then the request cannot be 
+   *
+   *  If the body of any request has content type
+   * `application/x-www-form-urlencoded`, then the request cannot be
    *  streaming as the body parameters are required as part of the signature.
    */
   Client(this.tokens, {http.BaseClient client}):
     _client = client != null ? client : new http.Client();
-    
+
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
       var nonce = getRandomBytes(8);
